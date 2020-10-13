f41018bc57ee13a4a4cfb814a1a596cfff8bc9e0
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2020-10-13 14:55:33 +1100

    Create ebs_snapshot.py

---
+++ ebs_snapshot.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+import boto3
+
+#def lambda_handler(event, context):  if use lambda 
+
+ec2_client = boto3.client('ec2')
+regions = [region['RegionName']
+               for region in ec2_client.describe_regions()['Regions']]
+
+for region in regions:
+    if region=='ap-south-1':
+        print('Instances in EC2 Region {0}:'.format(region))
+        ec2 = boto3.resource('ec2', region_name=region)
+
+        instances = ec2.instances.filter( Filters=[ {'Name': 'tag:backup', 'Values': ['true']},{'Name': 'instance-id', 'Values': ['i-046ae5c8d0ea12244']} ])
+        # ISO 8601 timestamp, i.e. 2019-01-31T14:01:58
+        timestamp = datetime.utcnow().replace(microsecond=0).isoformat()
+
+        for i in instances.all():
+            for v in i.volumes.all():
+
+                desc = 'Backup of {0}, volume {1}, created {2}'.format(
+                    i.id, v.id, timestamp)
+                print(desc)
+
+                snapshot = v.create_snapshot(Description=desc)
+
+                print("Created snapshot:", snapshot.id)
