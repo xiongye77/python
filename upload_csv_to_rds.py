e459e36c787392070b4ae689a645d7a5c045e782
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2020-11-04 20:49:53 +1100

    Create upload_csv_to_rds.py

---
+++ upload_csv_to_rds.py
@@ -0,0 +1,81 @@
+import psycopg2
+import boto3
+import os
+import sys
+import tempfile
+import csv
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+s3 = boto3.resource('s3')
+client = boto3.client('s3')
+
+def lambda_handler(event, context):
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    obj = s3.Object(bucket_name, key)
+    print (obj)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        download_path = os.path.join(tmpdir, key)
+        client.download_file(bucket_name, key, download_path)
+        print("Downloaded s3 file, {}, to {}".format(key, download_path))
+        #items = read_csv(download_path)
+        #for item in items:
+        #    print (item)
+        try:
+            connection = psycopg2.connect(user = "postgres",password = "Symantec123",host = "rdsproxy.proxy-ckzrjbfff4pm.us-east-1.rds.amazonaws.com",port = "5432", database = "postgres")
+
+            cur = connection.cursor()
+            with open(download_path,'r') as csvfile:
+                # skip header
+                #next(csvfile)
+                csv_data = csv.reader(csvfile,delimiter=',')
+                for row in csv_data:
+                    logger.info(row)
+                    cur.execute ('INSERT INTO customers(id,name) values ( %s ,  %s )',row)
+        
+                connection.commit()
+        
+        except (Exception, psycopg2.Error) as error :
+            print ("Error while connecting to PostgreSQL", error)
+    
+        finally:
+        #closing database connection.
+            if(connection):
+                cur.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+        
+        # Print PostgreSQL Connection properties
+        #print ( connection.get_dsn_parameters(),"\n")
+
+        # Print PostgreSQL version
+        #cursor.execute("SELECT version();")
+        #record = cursor.fetchone()
+        #print("You are connected to - ", record,"\n")
+
+        #    for idx,row in enumerate(csv_data):
+        #        logger.info(row)
+        #        try:
+        #            cur.execute('INSERT INTO customers(id, name) VALUES("%s", "%s")', row)
+        #        except Exception as e:
+        #            logger.error(e)
+        #        if idx % 100 == 0:
+        #            connection.commit()
+        #    connection.commit()
+    
+
+
+
+
+
+def read_csv(file):
+    items = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data = {}
+            data['ID'] = int(row['ID'])
+            data['Name'] = row['Name']
+            items.append(data)
+    return items
