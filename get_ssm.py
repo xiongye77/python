77f9ba451895c95e76de027944026efded3ad57d
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2021-07-29 16:22:07 +1000

    Create get_ssm.py

---
+++ get_ssm.py
@@ -0,0 +1,36 @@
+import boto3
+
+ssm_client = boto3.client('ssm')
+
+
+def get_parameters():
+    """Load parameter values from AWS Systems Manager (SSM) Parameter Store"""
+
+    params = {
+        'airflow_bucket': ssm_client.get_parameter(Name='/emr_demo/airflow_bucket')['Parameter']['Value'],
+        'bootstrap_bucket': ssm_client.get_parameter(Name='/emr_demo/bootstrap_bucket')['Parameter']['Value'],
+        'bronze_bucket': ssm_client.get_parameter(Name='/emr_demo/bronze_bucket')['Parameter']['Value'],
+        'cluster_id': ssm_client.get_parameter(Name='/emr_demo/cluster_id')['Parameter']['Value'],
+        'ec2_key_name': ssm_client.get_parameter(Name='/emr_demo/ec2_key_name')['Parameter']['Value'],
+        'ec2_subnet_id': ssm_client.get_parameter(Name='/emr_demo/ec2_subnet_id')['Parameter']['Value'],
+        'emr_ec2_role': ssm_client.get_parameter(Name='/emr_demo/emr_ec2_role')['Parameter']['Value'],
+        'emr_role': ssm_client.get_parameter(Name='/emr_demo/emr_role')['Parameter']['Value'],
+        'glue_db_bucket': ssm_client.get_parameter(Name='/emr_demo/glue_db_bucket')['Parameter']['Value'],
+        'gold_bucket': ssm_client.get_parameter(Name='/emr_demo/gold_bucket')['Parameter']['Value'],
+        'logs_bucket': ssm_client.get_parameter(Name='/emr_demo/logs_bucket')['Parameter']['Value'],
+        'master_public_dns': ssm_client.get_parameter(Name='/emr_demo/master_public_dns')['Parameter']['Value'],
+        'silver_bucket': ssm_client.get_parameter(Name='/emr_demo/silver_bucket')['Parameter']['Value'],
+        'sm_log_group_arn': ssm_client.get_parameter(Name='/emr_demo/sm_log_group_arn')['Parameter']['Value'],
+        'sm_role_arn': ssm_client.get_parameter(Name='/emr_demo/sm_role_arn')['Parameter']['Value'],
+        'vpc_id': ssm_client.get_parameter(Name='/emr_demo/vpc_id')['Parameter']['Value'],
+        'work_bucket': ssm_client.get_parameter(Name='/emr_demo/work_bucket')['Parameter']['Value']
+    }
+    print (params)
+    #return params
+
+def main():
+    get_parameters()
+
+if __name__ == '__main__':
+    main()
+~
